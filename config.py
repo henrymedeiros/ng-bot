e74bd1a9a6d4f656ccb50286ba52e20a23e9bd37
henry <henrymedeiros77@gmail.com>
2025-09-15 22:34:07 -0300

    fix: config_local import

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ DISCORD_BOT_TOKEN = "your-discord-bot-token-here"
 
 
 try:
-    from .config_local import *
+    from config_local import *
     print("Using config_local.py")
 except ImportError:
     print("No config_local.py, using default values.")
